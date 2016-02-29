95ea87c5d121ab3c5eb2b17e677c16dc76dcc6f7
Jose Lorente <jose.lorente.martin@gmail.com>
2016-02-29 12:03:26 +0100

    Added icon buttons and icon anchors to Html

--- src/Html.php
+++ src/Html.php
@@ -15,7 +15,7 @@ use yii\helpers\Html as BaseHtml;
  * 
  */
 class Html extends BaseHtml {
-    
+
     /**
      * 
      * @param string $class
@@ -24,4 +24,37 @@ class Html extends BaseHtml {
     public static function font($class) {
         return static::tag('i', '', ['class' => $class]);
     }
+
+    /**
+     * Renders an icon button.
+     * 
+     * @param string $icon
+     * @param array $options
+     * @return string
+     */
+    public static function iconButton($icon, array $options = []) {
+        if (isset($options['class'])) {
+            $options['class'] = 'icon ' . $options['class'];
+        } else {
+            $options['class'] = 'icon';
+        }
+        return static::tag('button', static::font($icon), $options);
+    }
+
+    /**
+     * Renders an incon anchor.
+     * 
+     * @param string $icon
+     * @param array $options
+     * @return string
+     */
+    public static function iconA($icon, $url, array $options = []) {
+        if (isset($options['class'])) {
+            $options['class'] = 'icon ' . $options['class'];
+        } else {
+            $options['class'] = 'icon';
+        }
+        return static::a(static::font($icon), $url, $options);
+    }
+
 }
